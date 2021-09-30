09f4b131c7d41bfb8b4bfb2e448724ed1d45ce0d
Marcus Osterberg <www.marcusosterberg.com>
2021-09-30 23:54:24 +0200

    add packages to setup

--- setup.sh
+++ setup.sh
@@ -11,4 +11,8 @@ npm install -g dockerfile-language-server-nodejs
 #zsh plugins
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/hlissner/zsh-autopair ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autopair
 
+# vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
